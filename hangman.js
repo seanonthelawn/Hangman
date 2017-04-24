c03e41e36efe13eb278f2b912b4369059e849440
seanonthelawn <seanonthelawn@gmail.com>
2017-04-24 17:24:17 -0500

    Add files via upload

--- hangman.js
+++ hangman.js
@@ -4,30 +4,56 @@ $(document).ready(function(){
 	
 	var random = Math.floor((Math.random()*(words.length)));
 	
-	var guessArray = Array();
+	var guessArray = [];
 	
-	var guessedArray = Array();
+	var guessedArray = [];
+	
+	var guessedChar;
+	
+	var badGuessCount = 0;
+	
+	var badGuessBool = true;
 	
 	for(var i = 0; i<words[random].length; i++){
-		guessArray += "_ ";
+		 guessArray.push("_ ");
 	}
 	
 	$("#word").html(guessArray);
 	
 	$("#guess").click(function() {
+	
+	guessedChar = ($("#guessZone").val().toUpperCase())
+		if(badGuessCount<=6){
+			guessedArray.push($("#guessZone").val().toUpperCase())
+			$("#guessed").html(guessedArray);
 		
-		guessedArray.push($("#guessZone").val());
-		$("#guessed").html(guessedArray);
-		
-		for(var i = 0; i<words[random].length; i++){
-			if($("#guessZone").val()== words[random].charAt(i)){
-				guessArray[i] = $("#guessZone").val();
+			for(var i = 0; i<words[random].length+1; i++){
+				console.log($("#guessZone").val().toUpperCase());
+				console.log(words[random].charAt(i-1).toUpperCase());
+				if($("#guessZone").val().toUpperCase() == words[random].charAt(i-1).toUpperCase()){
+					
+					console.log("Match: " + guessedChar);
+					guessArray.splice(i-1,1,guessedChar);
+					console.log(guessArray);
+					$("#word").html(guessArray);
+					badGuessBool = false;
+				} 
+				
+					
 			}
 			
+			if(badGuessBool == true){
+				badGuessCount++;
+				console.log(badGuessCount);
+				$("#badGuesses").html(badGuessCount);
+			}
+			
+			
+			
+			
 		}
-		
-		
-		
+		badGuessBool = true;
+	
 	});
 	
 	
